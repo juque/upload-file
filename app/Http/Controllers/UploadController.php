da4e2552188d4caec118d7f16be5ebee7f8ed374
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2023-10-24 17:11:06 -0300

    add messages: success or erros

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -6,12 +6,20 @@ use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('upload');
     }
 
-    public function store(Request $request) {
-        $attachment = $request->file('attachment')->store(options: 'attachments')
-        dd($attachment);
+    public function store(Request $request)
+    {
+        $request->validate([
+            'attachment' => 'required|max:5000'
+        ]);
+
+        $attachment = $request->file('attachment')->store(options: 'attachments');
+
+        return redirect()->route('upload')
+            ->withMessage("File Successfully uploaded");
     }
 }
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -2,6 +2,21 @@
   <h2>Upload form</h2>
   <form method="post" enctype="multipart/form-data" action="{{ route('upload.store')}}">
     @csrf
+
+    @if (session('message'))
+        <div class="alert">{{ session('message') }}</div>
+    @endif
+
+    @if ($errors->any())
+       <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <input type="file" name="attachment" />
     <input type="submit" value="Upload" />
   </form>
